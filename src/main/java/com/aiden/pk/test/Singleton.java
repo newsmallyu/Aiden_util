f9f6485bab4542ed7e72cff2d4a6b88211d17bfc
Aiden.Q.Yu <aiden.q.yu@newegg.com>
2021-05-25 17:47:58 +0800

    update

--- src/main/java/com/aiden/pk/test/Singleton.java
+++ src/main/java/com/aiden/pk/test/Singleton.java
@@ -1,7 +1,13 @@
 package com.aiden.pk.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * 懒加载单例模式
+ *
+ * 懒加载单例模式 系统启动时内部类是不会加载的。但当内部类第一次被调用时，则会成功初始化实例
  * @author ay05
  */
 public class Singleton {
@@ -11,4 +17,5 @@ public class Singleton {
     public static Singleton getSingleton(){
         return SingletonHolder.instance;
     }
+
 }
