be5cb307a9b624b71246318229ff6560acf36894
Aiden.Q.Yu <aiden.q.yu@newegg.com>
2021-07-14 16:03:29 +0800

    update

--- src/test/java/com/aiden/pk/AidenUtilApplicationTests.java
+++ src/test/java/com/aiden/pk/AidenUtilApplicationTests.java
@@ -1,18 +1,23 @@
 package com.aiden.pk;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.SortedSet;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SpringBootTest
 class AidenUtilApplicationTests {
 
-
+	private static Logger logger = LoggerFactory.getLogger(AidenUtilApplicationTests.class);
 	@Test
 	void contextLoads() {
 	}
@@ -57,5 +62,32 @@ class AidenUtilApplicationTests {
 	@Test
 	void testMod(){
 		System.out.println( 16895%16384);
+	}
+
+	@Test
+	void testCharLength(){
+		String a = "哈";
+		System.out.println(a.getBytes(StandardCharsets.UTF_8).length);
+
+		String b = "abc";
+		System.out.println(b.getBytes(StandardCharsets.UTF_8).length);
+
+		System.out.println(Integer.valueOf('a'));
+
+		System.out.println((char)97);
+	}
+
+	@Test
+	void threadPoolDemo(){
+		ThreadFactory nameThreadFactory = new ThreadFactoryBuilder().setNameFormat("threadPoolDemo" + "-%d").build();
+
+		ThreadPoolExecutor threadPool = new ThreadPoolExecutor(5, 10, 0L,
+				TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(1024), nameThreadFactory);
+
+		AtomicInteger atomicInteger = new AtomicInteger();
+
+		for (int i = 0; i < 6; i++) {
+			threadPool.execute(()-> logger.info(String.valueOf(atomicInteger.getAndIncrement())) );
+		}
 	}
 }
